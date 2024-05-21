51df7d5a3bfba107d24759aa324223335f258066
Jean Oliveira | Stone <jean.rangel@stone.com.br>
2024-05-21 20:00:34 -0300

    Exercício sobre Construtor, métodos especial, método, propriedade, método de instância

--- modelos/restaurante.py
+++ modelos/restaurante.py
@@ -2,25 +2,30 @@ class Restaurante: #classe
     restaurantes = []
     
     def __init__(self, nome, categoria):  #construtor   
-        self.nome = nome   #atributos da classe
-        self.categoria = categoria #atributos da classe
+        self._nome = nome .title()  #atributos da classe
+        self.categoria = categoria.upper() #atributos da classe
         self._ativo = False #atributos da classe PRIVADO(Protegido) deve usar property para modificar
         Restaurante.restaurantes.append(self)
         
     def __str__(self):
-       return f'{self.nome} | {self.categoria}' #retorna o que está escrito dentro do objeto e não o código do objeto
+       return f'{self._nome} | {self.categoria}' #retorna o que está escrito dentro do objeto e não o código do objeto
    
-    def listar_restaurantes():
-        print(f'{'Nome do Restaurante'} | {'Categoria'} | {'Status'}')
+    @classmethod
+    def listar_restaurantes(cls):
+        print(f'{'Nome do Restaurante'.ljust(25)} | {'Categoria'.ljust(25)} | {'Status'}')
         print()
         for restaurante in Restaurante.restaurantes:
-            print(f'{restaurante.nome.ljust(25)} | {restaurante.categoria.ljust(25)} | {restaurante.ativo}')
+            print(f'{restaurante._nome.ljust(25)} | {restaurante.categoria.ljust(25)} | {restaurante.ativo}')
             
     @property
     def ativo(self):
         return 'ativado' if self._ativo else 'desativado'
     
-restaurante_praca = Restaurante('Praça','Gourmet')
-restaurante_pizza = Restaurante('Pizza Express', 'Italiana')
+    def alternar_estado(self):
+        self._ativo = not self._ativo
+    
+restaurante_praca = Restaurante('praça','Gourmet')
+restaurante_praca.alternar_estado()
+restaurante_pizza = Restaurante('pizza express', 'Italiana')
 
 Restaurante.listar_restaurantes()
